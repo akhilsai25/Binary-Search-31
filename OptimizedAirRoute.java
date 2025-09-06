ac424d89d21cc927fe59d0633a2345584fd36321
akhilsai25 <38877327+akhilsai25@users.noreply.github.com>
2025-09-06 15:04:29 -0500

    Update OptimizedAirRoute.java

--- OptimizedAirRoute.java
+++ OptimizedAirRoute.java
@@ -1,31 +1,35 @@
 import java.util.ArrayList;
 import java.util.List;
 
-// This solution does a simple nested iteration on both loops and keep track of max optimized route. During the process, if a better path is found it will update the array list
+// This solution adds the smallest array into a tree map which maintains list based on descending order. We then iterate over through each element from the largest array and iterate over the map to get the max optimized route.
+// At any point if we find better route we update it.
 public class AirRouteOptimizer {
 
+    
     private static List<List<Integer>> getOptimizedRoute(int[][] arr1, int[][] arr2, int target) {
+        if(arr1.length>arr2.length) return getOptimizedRoute(arr2, arr1, target);
         List<List<Integer>> optimizedRoute = new ArrayList<>();
         int maxOptimizedValue = 0;
-        for(int i=0; i<arr1.length; i++) {
-            int id1 = arr1[i][0];
-            int cost = arr1[i][1];
-            for(int j=0; j< arr2.length; j++) {
-                int id2 = arr2[j][0];
-                int cost2 = arr2[j][1];
-                if(cost+cost2>target) continue;
-                else if(cost+cost2>=maxOptimizedValue) {
-                    maxOptimizedValue=cost2+cost;
-                    if(cost+cost2>maxOptimizedValue) optimizedRoute.clear();
-                    List<Integer> temp = new ArrayList<>();
-                    temp.add(id1);
-                    temp.add(id2);
-                    optimizedRoute.add(temp);
+        TreeMap<Integer, Integer> map = new TreeMap<>(Comparator.reverseOrder());
+        for(int i=0;i<arr1.length;i++) {
+            map.put(arr1[i][1], arr1[i][0]);
+        }
+        for (int i=0;i<arr2.length;i++) {
+            int[] temp = arr2[i];
+            for(Map.Entry<Integer, Integer> entry : map.entrySet()) {
+                if(entry.getKey()+temp[1]>target) continue;
+                else if(entry.getKey()+temp[1]>=maxOptimizedValue) {
+                    if(entry.getKey()+temp[1]==maxOptimizedValue) optimizedRoute.clear();
+                    List<Integer> tempList = new ArrayList<>();
+                    tempList.add(temp[0]);
+                    tempList.add(entry.getValue());
+                    optimizedRoute.add(tempList);
                 }
             }
         }
         return optimizedRoute;
     }
+
 
     public static void main(String args[])
     {
